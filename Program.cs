df2ff928ceccf8b55dce286332ca1c5192609af3
Haritha <73516759+HarithaVattikuti@users.noreply.github.com>
2026-06-04 12:13:15 -0500

    Correct syntax errors in Program.cs
    Fix syntax errors and add missing components.

--- Program.cs
+++ Program.cs
@@ -1,2 +1,15 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+# Console.WriteLine("Hello, World!");
+using System;
+
+namespace BrokenSample;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        string text = 123;
+        Console.WriteLine(DoesNotExist);
+        MissingMethod(
+    }
+}
